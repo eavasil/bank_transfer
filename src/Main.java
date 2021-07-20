b63e79f698111a0dcb35d7a86e4d4fb5c3eb587b
eavasil <dan_kat@mail.ru>
2021-07-20 22:40:35 +0700

    ввод суммы перевода средств со счета

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        int account_balance = 2_000_000_000;
-        int transfer_from = 500_000_000;
-        int current_balance = account_balance - transfer_from;
-        System.out.println(current_balance);
+    public static void main(String[] args) throws Exception {
+        double account_balance = 2000000000;
+
+        System.out.println("Введите сумму перевода:");
+        Scanner scanner = new Scanner(System.in);
+        double transfer_from = (double) scanner.nextDouble();
+
+        System.out.println("Сумма перевода: " + transfer_from);
+
+        double current_balance = account_balance - transfer_from;
+
+        System.out.println("Остаток средств на счете составлет: " + current_balance);
     }
 }
